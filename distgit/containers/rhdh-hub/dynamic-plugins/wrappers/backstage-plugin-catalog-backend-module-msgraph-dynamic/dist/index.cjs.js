c78c23b1a07b1bf17cbbcc291f5721a50a0a527e
RHDH Build (rhdh-bot) <rhdh-bot@redhat.com>
2024-06-28 19:52:54 +0000

    chore: Update: rhdh-hub from: https://github.com/janus-idp/backstage-showcase/tree/main @ 6202da47; [skip ci]
    Signed-off-by: RHDH Build (rhdh-bot) <rhdh-bot@redhat.com>

---
+++ distgit/containers/rhdh-hub/dynamic-plugins/wrappers/backstage-plugin-catalog-backend-module-msgraph-dynamic/dist/index.cjs.js
@@ -0,0 +1,17 @@
+'use strict';
+
+Object.defineProperty(exports, '__esModule', { value: true });
+
+var alpha = require('@backstage/plugin-catalog-backend-module-msgraph/alpha');
+
+function _interopDefaultLegacy (e) { return e && typeof e === 'object' && 'default' in e ? e : { 'default': e }; }
+
+var alpha__default = /*#__PURE__*/_interopDefaultLegacy(alpha);
+
+
+
+Object.defineProperty(exports, 'default', {
+	enumerable: true,
+	get: function () { return alpha__default["default"]; }
+});
+//# sourceMappingURL=index.cjs.js.map
