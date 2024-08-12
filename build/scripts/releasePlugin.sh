3887ab85924590076366c635380ea0609b93464a
Nick Boldt <nboldt@redhat.com>
2024-08-12 15:21:33 -0300

    [ci skip] document how to do manual releases of plugins w/ multiple PRs and runs of push.yaml
    Signed-off-by: Nick Boldt <nboldt@redhat.com>

--- build/scripts/releasePlugin.sh
+++ build/scripts/releasePlugin.sh
@@ -7,7 +7,7 @@
 #
 # SPDX-License-Identifier: EPL-2.0
 #
-# Utility script to release a single plugin when multi-semantic-release (MSR) fails
+# Utility script to release 1 or more plugins when multi-semantic-release (MSR) fails
 # Will create a release on npmjs.com and tag the source repo with @scope/plugin-name@version 
 # so that MSR will think the plugin already exists and will skip it
 
@@ -22,7 +22,37 @@ if [[ ! $NPM_TOKEN ]]; then echo "NPM_TOKEN not set!"; exit 1; fi
 if [[ ! $GITHUB_TOKEN ]]; then echo "GITHUB_TOKEN not set!"; exit 1; fi
 
 usage () {
-    echo "Usage: $0 [--dry-run] -d /path/to/plugins-project plugins/some-plugin-dir1 [plugins/some-plugin-dir2...]";
+    echo "\
+Utility script to release 1 or more plugins when multi-semantic-release (MSR) fails
+Will create a release on npmjs.com and tag the source repo with @scope/plugin-name@version 
+so that MSR will think the plugin already exists and will skip it
+
+To use this script, there are a number of steps.
+
+Step 1: get sources
+
+* if the plugin is already merged to the repo, fetch the latest changes on the main branch; OR
+* if the plugin is being added via a PR, check out the PR sources locally
+
+Step 2: release manually
+
+* Locally, remove private:true from the package.json (do not commit this change!)
+* release manually using this script
+
+Step 3: release other plugins while the new private plugin
+* put private:true back in the package.json
+* regen yarn.lock (or use https://github.com/janus-idp/backstage-plugins/actions/workflows/yarn-lock.yaml)
+* submit a PR for the above changes
+* merge the PR and let MSR release everything EXCEPT the new private plugin from https://github.com/janus-idp/backstage-plugins/actions/workflows/push.yaml
+
+Step 4: release the new plugin (non-private)
+* If https://github.com/janus-idp/backstage-plugins/actions/workflows/push.yaml is green, 
+* Remove private:true from the package.json, and submit the PR
+* Merge changes & let MSR release just that plugin, updating any plugin refs and yarn.lock if needed
+
+--
+
+Usage: $0 [--dry-run] -d /path/to/plugins-project plugins/some-plugin-dir1 [plugins/some-plugin-dir2...]";
 }
 
 while [[ "$#" -gt 0 ]]; do
