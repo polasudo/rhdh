1d455d3e2f6dc910a57b33c73a11b5c5dd0f9860
RHDH Build (rhdh-bot) <rhdh-bot@redhat.com>
2024-11-18 17:47:19 +0000

    chore: Update: rhdh-hub from: https://github.com/janus-idp/backstage-showcase/tree/main @ 667ad944;
    Signed-off-by: RHDH Build (rhdh-bot) <rhdh-bot@redhat.com>

---
+++ distgit/containers/rhdh-hub/dynamic-plugins/wrappers/backstage-community-scaffolder-backend-module-regex-dynamic/dist/index.cjs.js
@@ -0,0 +1,17 @@
+'use strict';
+
+Object.defineProperty(exports, '__esModule', { value: true });
+
+var scaffolderBackendModuleRegex = require('@backstage-community/scaffolder-backend-module-regex');
+
+function _interopDefaultLegacy (e) { return e && typeof e === 'object' && 'default' in e ? e : { 'default': e }; }
+
+var scaffolderBackendModuleRegex__default = /*#__PURE__*/_interopDefaultLegacy(scaffolderBackendModuleRegex);
+
+
+
+Object.defineProperty(exports, 'default', {
+	enumerable: true,
+	get: function () { return scaffolderBackendModuleRegex__default["default"]; }
+});
+//# sourceMappingURL=index.cjs.js.map
