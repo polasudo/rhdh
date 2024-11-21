8aa63c2f9c93d5db5836b0453c13e32c5b8c5e28
RHDH Build (rhdh-bot) <rhdh-bot@redhat.com>
2024-11-21 02:49:39 +0000

    chore: Update: rhdh-hub from: https://github.com/janus-idp/backstage-showcase/tree/main @ 2b0779d2;
    Signed-off-by: RHDH Build (rhdh-bot) <rhdh-bot@redhat.com>

--- distgit/containers/rhdh-hub/dynamic-plugins/wrappers/pagerduty-backstage-plugin-backend-dynamic/dist/index.cjs.js
+++ distgit/containers/rhdh-hub/dynamic-plugins/wrappers/pagerduty-backstage-plugin-backend-dynamic/dist/index.cjs.js
@@ -4,14 +4,14 @@ Object.defineProperty(exports, '__esModule', { value: true });
 
 var backstagePluginBackend = require('@pagerduty/backstage-plugin-backend');
 
-function _interopDefaultLegacy (e) { return e && typeof e === 'object' && 'default' in e ? e : { 'default': e }; }
+function _interopDefaultCompat (e) { return e && typeof e === 'object' && 'default' in e ? e : { default: e }; }
 
-var backstagePluginBackend__default = /*#__PURE__*/_interopDefaultLegacy(backstagePluginBackend);
+var backstagePluginBackend__default = /*#__PURE__*/_interopDefaultCompat(backstagePluginBackend);
 
 
 
-Object.defineProperty(exports, 'default', {
+Object.defineProperty(exports, "default", {
 	enumerable: true,
-	get: function () { return backstagePluginBackend__default["default"]; }
+	get: function () { return backstagePluginBackend__default.default; }
 });
 //# sourceMappingURL=index.cjs.js.map
