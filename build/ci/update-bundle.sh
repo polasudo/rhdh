b7bdbe33bebd5e07112cb59f024893d522302d58
Nick Boldt <nboldt@redhat.com>
2025-01-23 21:29:23 -0500

    chore: Update: rhdh-hub from: https://github.com/redhat-developer/rhdh/tree/main @ 9a7353c3; rhdh-operator from: https://github.com/redhat-developer/rhdh-operator/tree/main @ ea81f3ea; rhdh-operator-bundle from: https://github.com/redhat-developer/rhdh-operator/tree/main @ ea81f3ea;
    Signed-off-by: Nick Boldt <nboldt@redhat.com>

--- build/ci/update-bundle.sh
+++ build/ci/update-bundle.sh
@@ -10,6 +10,9 @@
 # set -x
 set -e
 
+export RHDH_HUB="quay.io/rhdh/rhdh-hub-rhel9@sha256:1c2fead5406f7c1c164efa83b56210839bc296400284d3ca80753ccdc08f274a"
+export RHDH_OPERATOR="quay.io/rhdh/rhdh-rhel9-operator@sha256:9539680c13deaac90cd6846bd5a39d5ce593eb92b6ce377076de2f09eb9dcc33"
+    
 SCRIPTPATH=$(dirname "$(readlink -f "$0")")
 
 DWNSTM_BRANCH=$(git rev-parse --abbrev-ref HEAD 2>/dev/null || echo "rhdh-1-rhel-9")
