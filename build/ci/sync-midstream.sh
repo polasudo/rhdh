7091356c62ccdac9c3f8e8715684fcff95d98e0a
debsmita1 <debsmita.santra@gmail.com>
2025-03-25 18:17:59 +0530

    update build info script

--- build/ci/sync-midstream.sh
+++ build/ci/sync-midstream.sh
@@ -604,7 +604,7 @@ for ((i = START_REPO; i < NUM_REPOS; i++)); do # echo $i
 
       # set build-metadata.json info, using upstream info: ${ROOTPATH}/sync/upstream_SHA_rhdh-hub ==> redhat-developer/rhdh main @ 2ff35695
       sed -i packages/app/src/build-metadata.json -r \
-        -e 's|"(Last Commit: )(.+)"|"Upstream: '"$upstream_repo_hub"'", "Midstream: '"$midstream_repo_and_SHA"'", "Build Time: '"$now"'"|'
+        -e 's|"Last Commit": "(.+)"|"Upstream": "'"$upstream_repo_hub"'", "Midstream": "'"$midstream_repo_and_SHA"'", "Build Time": "'"$now"'"|'
     fi
     ##################################### rhdh-hub #####################################
 
