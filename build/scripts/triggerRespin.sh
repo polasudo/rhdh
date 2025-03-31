69055b6496e2715ffe98f0b82f533174b5171867
rhdh-bot service account <rhdh-bot@redhat.com>
2025-03-31 16:58:41 -0300

    revise build/scripts/triggerRespin.sh to run a --bundleonly build for 'bun' [ci skip]
    Signed-off-by: rhdh-bot service account <rhdh-bot@redhat.com>

--- build/scripts/triggerRespin.sh
+++ build/scripts/triggerRespin.sh
@@ -29,10 +29,10 @@ or
     $0 VERSION IMAGE1[,IMAGE2][,IMAGE3] 
 
 Examples: 
-    $0 1.2 hub,op,bun
-    $0 1.3 bun
-    $0 1 all
-
+    $0 1 hub,op  # both hub and operator (NOT bundle)
+    $0 1.6 hub   # only hub
+    $0 1.6 op    # only operator
+    $0 1.6 bun   # only bundle
 "; 
 }
 
@@ -43,41 +43,56 @@ fi
 
 MIDSTM_BRANCH=rhdh-${1}-rhel-9
 targets="$2"
-if [[ $targets == *","* ]]; then
-    commitMsg="trigger ${MIDSTM_BRANCH} builds: $targets"
-else
-    commitMsg="trigger ${MIDSTM_BRANCH} build: $targets"
-fi
-echo "$commitMsg ..."
-if [[ $targets == "all" ]]; then targets="hub,op,bun"; fi
 
-targets=${targets/hub/upstream_SHA_rhdh-hub}
-targets=${targets/operator/upstream_SHA_rhdh-operator}
-targets=${targets/op/upstream_SHA_rhdh-operator}
-targets=${targets/bundle/upstream_SHA_rhdh-operator-bundle}
-targets=${targets/bun/upstream_SHA_rhdh-operator-bundle}
-targets=${targets//,/ }
-if [[ -d "$ROOT_DIR/sync" ]]; then 
-    cd "$ROOT_DIR" || exit 1
-else
-    cd /tmp || exit 1
-    rm -fr /tmp/rhdh-tmp
-    git clone git@gitlab.cee.redhat.com:rhidp/rhdh.git rhdh-tmp || exit 1
-    cd rhdh-tmp || exit 1
-    git checkout "$MIDSTM_BRANCH" || exit 1
-fi
-git checkout "${MIDSTM_BRANCH}"
-git pull origin "${MIDSTM_BRANCH}"
-
-for target in $targets; do 
-    if [[ -f sync/$target ]]; then
-        echo "" > "sync/$target"
+if [[ $targets == "bun" ]]; then
+    latestStableBranch="$(curl -sSLk --url "https://gitlab.cee.redhat.com/api/v4/projects/rhidp%2Frhdh/repository/branches?per_page=200&regex=^rhdh-1..*-rhel-9$" | jq -r '.[].name' | sort -uV | tail -1)"; # echo $latestStableBranch
+    latestNext=""
+    if [[ ${DWNSTM_BRANCH} == "rhdh-"*"-rhel-"* ]]; then 
+        if [[ $DWNSTM_BRANCH == "rhdh-1-rhel-9" ]]; then
+            latestNext="--next"
+        elif [[ "$DWNSTM_BRANCH" == "${latestStableBranch}" ]]; then # latest stable branch
+            latestNext="--latest"
+        fi
     fi
-done
-git diff --name-status sync/
-git commit -s -m "$commitMsg" sync/
-git push origin "${MIDSTM_BRANCH}"
-google-chrome https://gitlab.cee.redhat.com/rhidp/rhdh/-/pipelines
+    "${SCRIPT_DIR}/../ci/sync-midstream.sh" --bundleonly --force $latestNext -b "${MIDSTM_BRANCH}"
+else
+    if [[ $targets == *","* ]]; then
+        commitMsg="trigger ${MIDSTM_BRANCH} builds: $targets"
+    else
+        commitMsg="trigger ${MIDSTM_BRANCH} build: $targets"
+    fi
+    echo "$commitMsg ..."
+    if [[ $targets == "all" ]]; then targets="hub,op"; fi
 
-# cleanup
-if [[ -d /tmp/rhdh-tmp ]]; then rm -fr /tmp/rhdh-tmp; fi
+    targets=${targets/hub/upstream_SHA_rhdh-hub}
+    targets=${targets/operator/upstream_SHA_rhdh-operator}
+    targets=${targets/op/upstream_SHA_rhdh-operator}
+    targets=${targets/bundle/upstream_SHA_rhdh-operator-bundle}
+    targets=${targets/bun/upstream_SHA_rhdh-operator-bundle}
+    targets=${targets//,/ }
+    if [[ -d "$ROOT_DIR/sync" ]]; then 
+        cd "$ROOT_DIR" || exit 1
+    else
+        cd /tmp || exit 1
+        rm -fr /tmp/rhdh-tmp
+        git clone git@gitlab.cee.redhat.com:rhidp/rhdh.git rhdh-tmp || exit 1
+        cd rhdh-tmp || exit 1
+        git checkout "$MIDSTM_BRANCH" || exit 1
+    fi
+    git checkout "${MIDSTM_BRANCH}"
+    git pull origin "${MIDSTM_BRANCH}"
+
+    for target in $targets; do 
+        if [[ -f sync/$target ]]; then
+            echo "" > "sync/$target"
+        fi
+    done
+    git diff --name-status sync/
+    git commit -s -m "$commitMsg" sync/
+    git push origin "${MIDSTM_BRANCH}"
+
+    # cleanup
+    if [[ -d /tmp/rhdh-tmp ]]; then rm -fr /tmp/rhdh-tmp; fi
+fi
+
+google-chrome https://gitlab.cee.redhat.com/rhidp/rhdh/-/pipelines
