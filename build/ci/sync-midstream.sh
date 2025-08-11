186faac9f7b66817ab123f4e9536b33d3352823e
Nick Boldt <nboldt@redhat.com>
2025-08-11 15:42:34 -0300

    don't install rpm-lockfile-prototype if not needed
    Signed-off-by: Nick Boldt <nboldt@redhat.com>

--- build/ci/sync-midstream.sh
+++ build/ci/sync-midstream.sh
@@ -1028,11 +1028,13 @@ for d in $these_dirs; do
     ##################################### update the RPM lock files to make Cachi2 and ECP happy ##################################### 
     if [[ $d == "distgit/containers/rhdh-hub" ]] || [[ $d == "distgit/containers/rhdh-operator" ]]; then
       if [[ -f rpms.in.yaml ]] && [[ -f rpms.lock.yaml ]]; then
-        echo "Install rpm-lockfile-prototype ... "
-        sudo dnf -q -y install python3 python3-pip python3-dnf
-        mkdir -p "${HOME}/.local/bin/"
-        python3 -m pip install --user https://github.com/konflux-ci/rpm-lockfile-prototype/archive/refs/heads/main.zip
-        export PATH=${PATH%":${HOME}/.local/bin"}:${HOME}/.local/bin
+        if [[ ! -x "${HOME}/.local/bin/rpm-lockfile-prototype" ]]; then 
+          echo "Install rpm-lockfile-prototype ... "
+          sudo dnf -q -y install python3 python3-pip python3-dnf
+          mkdir -p "${HOME}/.local/bin/"
+          python3 -m pip install --user https://github.com/konflux-ci/rpm-lockfile-prototype/archive/refs/heads/main.zip
+          export PATH=${PATH%":${HOME}/.local/bin"}:${HOME}/.local/bin
+        fi
         echo "[INFO] Regen $d/rpms.lock.yaml from Containerfile + rpms.in.yaml using $(which rpm-lockfile-prototype) in [$(pwd)]"
         "${HOME}/.local/bin/rpm-lockfile-prototype" -f Containerfile rpms.in.yaml # >/dev/null 2>&1 
       fi
