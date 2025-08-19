d0e451a35339778fbe36006444828a26aea143b7
rhdh-bot service account <rhdh-bot@redhat.com>
2025-08-19 14:02:42 -0300

    [ci skip] track how long rpm-lockfile-prototype takes to run
    Signed-off-by: rhdh-bot service account <rhdh-bot@redhat.com>

--- build/ci/sync-midstream.sh
+++ build/ci/sync-midstream.sh
@@ -1042,13 +1042,13 @@ for d in $these_dirs; do
       if [[ -f rpms.in.yaml ]] && [[ -f rpms.lock.yaml ]]; then
         if [[ ! -x "${HOME}/.local/bin/rpm-lockfile-prototype" ]]; then 
           echo "Install rpm-lockfile-prototype ... "
-          sudo dnf -q -y install python3 python3-pip python3-dnf
+          time sudo dnf -q -y install python3 python3-pip python3-dnf
           mkdir -p "${HOME}/.local/bin/"
-          python3 -m pip install --user https://github.com/konflux-ci/rpm-lockfile-prototype/archive/refs/heads/main.zip
+          time python3 -m pip install --user https://github.com/konflux-ci/rpm-lockfile-prototype/archive/refs/heads/main.zip
           export PATH=${PATH%":${HOME}/.local/bin"}:${HOME}/.local/bin
         fi
         echo "[INFO] Regen $d/rpms.lock.yaml from Containerfile + rpms.in.yaml using $(which rpm-lockfile-prototype) in [$(pwd)]"
-        "${HOME}/.local/bin/rpm-lockfile-prototype" -f Containerfile rpms.in.yaml # >/dev/null 2>&1 
+        time "${HOME}/.local/bin/rpm-lockfile-prototype" -f Containerfile rpms.in.yaml # >/dev/null 2>&1 
       fi
     fi
 
