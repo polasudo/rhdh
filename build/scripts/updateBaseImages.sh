33369484b3d32471a14e5191ba073b3b341d70f8
Nick Boldt <nboldt@redhat.com>
2025-11-06 14:40:08 -0400

    [ci skip] updateBaseImages.sh use lowercase in commit message to avoid the wrath of the semantic commit message test

--- build/scripts/updateBaseImages.sh
+++ build/scripts/updateBaseImages.sh
@@ -353,7 +353,7 @@ for d in $(find "${WORKDIR}/" -maxdepth "${MAXDEPTH}" -name "${DOCKERFILE}" | so
 								if [[ -d ${d%%/${DOCKERFILE}} ]]; then pushd "${d%%/${DOCKERFILE}}" >/dev/null; pushedIn=1; fi
 								if [[ ${docommit} -eq 1 ]]; then 
 									git add "${DOCKERFILE}" || true
-									git commit -s -m "chore: Update to ${FROMPREFIX}:${LATESTTAG} from ${URL} [skip-build]" "${DOCKERFILE}" 2>&1 || true
+									git commit -s -m "chore: update to ${FROMPREFIX}:${LATESTTAG} from ${URL} [skip-build]" "${DOCKERFILE}" 2>&1 || true
 									if [[ ${dopronly} -eq 1 ]]; then
 										createPr "${PR_BRANCH}" "${SOURCES_BRANCH}"
 									else
